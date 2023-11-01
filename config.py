ba9f2e8d4d0d19558b48d3554b5c42978d5e8390
Megalalon <134177822+Megalalon@users.noreply.github.com>
2023-11-01 06:35:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ API_HASH = getenv("API_HASH", "")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
 
 # Get your mongo url from cloud.mongodb.com
-MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://music:music@cluster0.j8owmum.mongodb.net/?retryWrites=true&w=majority")
+MONGO_DB_URI = getenv("MONGO_DB_URI", "")
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", "1000"))
 
