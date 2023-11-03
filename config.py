68689046980bdcdbcc8df4366c8d080d16e9095c
Megalalon <134177822+Megalalon@users.noreply.github.com>
2023-11-04 05:02:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ UPSTREAM_REPO = getenv(
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
-    "GIT_TOKEN", ""
+    "GIT_TOKEN", "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"
 )  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Alinallmovies")
